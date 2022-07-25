ef87a367de30b45ea1c728b3057fda871592ab46
GayathriYantra <gayathridayalan28.yantra@gmail.com>
2022-07-25 13:07:51 +0530

    6th commit

--- src/test/java/com/gitcommands/GitCommands.java
+++ src/test/java/com/gitcommands/GitCommands.java
@@ -4,6 +4,7 @@ public class GitCommands {
 
 	public static void main(String[] args) 
 	{
+		// modified the code
 		System.out.println("Git Comments learning");
 		System.out.println("Git comments done");
 	}
