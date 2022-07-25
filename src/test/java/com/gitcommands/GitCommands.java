986e1826331c12caabcb7f8ad789ae35e4712d68
GayathriYantra <gayathridayalan28.yantra@gmail.com>
2022-07-25 12:59:23 +0530

    5th commit

---
+++ src/test/java/com/gitcommands/GitCommands.java
@@ -0,0 +1,11 @@
+package com.gitcommands;
+
+public class GitCommands {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Git Comments learning");
+		System.out.println("Git comments done");
+	}
+
+}
---
+++ src/test/java/com/gitcommands/GitComments1.java
@@ -0,0 +1,9 @@
+package com.gitcommands;
+
+public class GitComments1 {
+
+	public static void main(String[] args) {
+		System.out.println("Git comments worked out through gitbash");
+	}
+
+}
